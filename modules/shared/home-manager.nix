0d111a9f3af8596e46f8f4692c348be544d6e303
david <xlrin.morgan@gmail.com>
2024-08-02 19:27:58 -0400

    merged

---
+++ modules/shared/home-manager.nix
@@ -0,0 +1,58 @@
+{ config, pkgs, lib, ... }:
+
+let 
+  name = "david";
+  user = "david";
+  email = "xlrin.morgan@gmail.com"; 
+in
+{
+  # Shared shell configuration
+  zsh = {
+    enable = true;
+    autocd = false;
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = lib.cleanSource ./config;
+        file = "p10k.zsh";
+      }
+    ];
+  };
+
+
+programs.neovim = {
+    extraPackages = with pkgs; [
+      # LazyVim
+      lua-language-server
+      stylua
+      # Telescope
+      ripgrep
+    ];
+
+
+  git = {
+    enable = true;
+    ignores = [ "*.swp" ];
+    userName = name;
+    userEmail = email;
+    lfs = {
+      enable = true;
+    };
+    extraConfig = {
+      init.defaultBranch = "main";
+      core = {
+        editor = "nvim";
+        autocrlf = "input";
+      };
+      pull.rebase = true;
+      rebase.autoStash = true;
+    };
+  };
+
+  }
+
