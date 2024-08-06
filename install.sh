d8fd7964029d84710bb3e1f4c5afa617433bd426
david <xlrin.morgan@gmail.com>
2024-08-05 21:29:44 -0400

    added installer

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+
+read -p "Press [Enter] key to continue..."
+
+echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
+chmod -R 755 ~/nix
+find apps/$(uname -m | sed 's/arm64/aarch64/')-darwin -type f \( -name apply -o -name build -o -name build-switch -o -name create-keys -o -name copy-keys -o -name check-keys \) -exec chmod +x {} \;
+nix run .#build 
+
+read -p "Press [Enter] key to continue..."
+
+nix run .#build-switch
