bad2edaee5d191a7c0fee2bb8678c93907fb64c1
david <xlrin.morgan@gmail.com>
2025-03-08 10:30:35 -0500

    fixed again

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -196,7 +196,7 @@ in
               "nvim-telescope/telescope.nvim"
             },
             config = function()
-              -- vim.env.OPENAI_API_KEY = "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"
+              -- vim.env.OPENAI_API_KEY = ""
               require("chatgpt").setup({
                 openai_params = {
                   -- model = "gpt-4",
