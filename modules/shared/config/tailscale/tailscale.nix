e99bf24670f2befc6617c0a19493e1debef1a83d
XLRINN <63594307+XLRINN@users.noreply.github.com>
2025-07-23 19:47:53 +0000

    added key

--- modules/shared/config/tailscale/tailscale.nix
+++ modules/shared/config/tailscale/tailscale.nix
@@ -5,7 +5,8 @@
     enable = true;
     useRoutingFeatures = "client"; # or "both" for subnet routing
     extraUpFlags = [ "--ssh" ];
+     authKeyFile = ./key.txt;
   };
 
-  networking.firewall.allowedUDPPorts = [ 41641 ];
+  #networking.firewall.allowedUDPPorts = [ 41641 ];
 }
