acb9ab7394071378d293756d87766313dcf4f593
david <xlrin.morgan@gmail.com>
2025-07-28 12:15:42 -0400

    Add Lucide Icons as a replacement for Feather Icons in the icon font overlay, enhancing the icon toolkit with a consistent and beautiful design. Update installation phase to copy font files and include metadata for the new package.

--- overlays/10-feather-font.nix
+++ overlays/10-feather-font.nix
@@ -1,12 +1,30 @@
 # Icon Font Overlay - Using reliable alternatives to Feather Font
 self: super: with super; {
-  # This overlay can be used to add custom icon fonts if needed
-  # For now, we'll use existing nixpkgs icon fonts instead
-  
-  # Example: If you want to add a custom icon font later, add it here
-  # custom-icon-font = stdenv.mkDerivation {
-  #   name = "custom-icon-font-1.0";
-  #   src = fetchFromGitHub { ... };
-  #   ...
-  # };
+  # Lucide Icons - Direct successor to Feather Icons
+  lucide-icons = stdenv.mkDerivation {
+    name = "lucide-icons-0.263.1";
+    
+    src = fetchFromGitHub {
+      owner = "lucide-icons";
+      repo = "lucide";
+      rev = "v0.263.1";
+      sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # Will need to update this
+    };
+
+    buildInputs = [ ];
+    phases = [ "unpackPhase" "installPhase" ];
+
+    installPhase = ''
+      mkdir -p $out/share/fonts/truetype
+      # Copy any font files if they exist
+      find . -name "*.ttf" -o -name "*.otf" | xargs -I {} cp {} $out/share/fonts/truetype/ || true
+    '';
+
+    meta = with lib; {
+      description = "Lucide Icons - Beautiful & consistent icon toolkit (Feather successor)";
+      homepage = "https://lucide.dev";
+      license = licenses.mit;
+      platforms = platforms.all;
+    };
+  };
 }
