1a8717d4b868c2a932e617cdc3f3475639d3b02b
david <xlrin.morgan@gmail.com>
2025-07-28 11:24:44 -0400

    Simplify disk config to ext4 for testing install

--- modules/nixos/disk-config.nix
+++ modules/nixos/disk-config.nix
@@ -1,5 +1,5 @@
 _: {
-  # Btrfs configuration with hibernation support
+  # Simple ext4 configuration (like Dustin's)
   disko.devices = {
     disk = {
       vdb = {
@@ -20,72 +20,9 @@ _: {
             root = {
               size = "100%";
               content = {
-                type = "btrfs";
-                extraArgs = [ "-f" ];
+                type = "filesystem";
+                format = "ext4";
                 mountpoint = "/";
-                mountOptions = [
-                  "defaults"
-                  "noatime"
-                  "compress=zstd"
-                  "space_cache=v2"
-                  "subvol=@"
-                ];
-                subvolumes = {
-                  "@" = {
-                    mountpoint = "/";
-                    mountOptions = [
-                      "defaults"
-                      "noatime"
-                      "compress=zstd"
-                      "space_cache=v2"
-                    ];
-                  };
-                  "@home" = {
-                    mountpoint = "/home";
-                    mountOptions = [
-                      "defaults"
-                      "noatime"
-                      "compress=zstd"
-                      "space_cache=v2"
-                    ];
-                  };
-                  "@nix" = {
-                    mountpoint = "/nix";
-                    mountOptions = [
-                      "defaults"
-                      "noatime"
-                      "compress=zstd"
-                      "space_cache=v2"
-                    ];
-                  };
-                  "@var" = {
-                    mountpoint = "/var";
-                    mountOptions = [
-                      "defaults"
-                      "noatime"
-                      "compress=zstd"
-                      "space_cache=v2"
-                    ];
-                  };
-                  "@tmp" = {
-                    mountpoint = "/tmp";
-                    mountOptions = [
-                      "defaults"
-                      "noatime"
-                      "compress=zstd"
-                      "space_cache=v2"
-                    ];
-                  };
-                  "@swap" = {
-                    mountpoint = "/swap";
-                    mountOptions = [
-                      "defaults"
-                      "noatime"
-                      "compress=zstd"
-                      "space_cache=v2"
-                    ];
-                  };
-                };
               };
             };
           };
