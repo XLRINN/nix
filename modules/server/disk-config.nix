fc5740346b787100f52c8e6978897c4e2a97bb3f
david <xlrin.morgan@gmail.com>
2025-08-07 22:54:25 -0400

    Add manual disk partitioning alternatives to Disko

--- modules/server/disk-config.nix
+++ modules/server/disk-config.nix
@@ -1,3 +1,30 @@
+# Single partition configuration (alternative)
+# Uncomment this and comment out the multi-partition config below for simple setup
+# {
+#   disko.devices = {
+#     disk = {
+#       sda = {
+#         device = "/dev/sda";
+#         type = "disk";
+#         content = {
+#           type = "msdos";
+#           partitions = {
+#             root = {
+#               size = "100%";
+#               content = {
+#                 type = "filesystem";
+#                 format = "ext4";
+#                 mountpoint = "/";
+#               };
+#             };
+#           };
+#         };
+#       };
+#     };
+#   };
+# }
+
+# Multi-partition configuration (current)
 {
   disko.devices = {
     disk = {
