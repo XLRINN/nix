3782dd690aae8bbf110d4004fb525ee14c584935
XLRINN <63594307+XLRINN@users.noreply.github.com>
2025-08-12 15:32:54 +0000

    fix: simplify home-manager.nix to minimum configuration

--- modules/server/home-manager.nix
+++ modules/server/home-manager.nix
@@ -1,24 +1,10 @@
 { config, pkgs, lib, ... }:
 
-let
-  user = "david";
-in
 {
-  # imports = [
-  #   # CLI tools only - commented out for initial installation testing
-  #   ../shared/config/terminal/git.nix
-  #   ../shared/config/terminal/zsh.nix
-  #   ../shared/config/terminal/neovim.nix
-  #   ../shared/config/terminal/tmux.nix
-  #   ../shared/config/terminal/zellij.nix
-  #   ../shared/config/terminal/direnv.nix
-  #   ../shared/config/terminal/monitoring.nix
-  # ];
-
   home = {
     enableNixpkgsReleaseCheck = false;
-    username = "${user}";
-    homeDirectory = "/home/${user}";
+    username = "david";
+    homeDirectory = "/home/david";
     stateVersion = "23.11";
   };
 }
