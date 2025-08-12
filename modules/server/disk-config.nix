fc2499a7cce0292ac2f1a43bfcb1cc57bc6e3147
XLRINN <63594307+XLRINN@users.noreply.github.com>
2025-08-12 17:57:05 -0400

    Make disk-config.nix universal for BIOS: adds ext4 /boot and bootable partition, works everywhere.

--- modules/server/disk-config.nix
+++ modules/server/disk-config.nix
@@ -1,5 +1,4 @@
-# BIOS-only configuration matching the structure of working nixos config
-{
+{ 
   disko.devices.disk.main = {
     device = "/dev/sda";
     type = "disk";
@@ -8,7 +7,16 @@
       partitions = {
         bios = {
           size = "1M";
-          type = "EF02";  # BIOS boot partition for GPT
+          type = "EF02"; # BIOS boot partition for GPT
+        };
+        boot = {
+          size = "512M";
+          type = "8300"; # Linux filesystem
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/boot";
+          };
         };
         root = {
           size = "100%";
