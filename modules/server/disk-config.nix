e8e1f4859808a1e68b3ebf584c00cc4e5fe112d0
david <xlrin.morgan@gmail.com>
2025-08-13 10:05:28 -0400

    feat: add disk-config.nix from desktop branch with 100M ESP and mountOptions

--- modules/server/disk-config.nix
+++ modules/server/disk-config.nix
@@ -1,29 +1,36 @@
-{
-  disko.devices.disk.main = {
-    device = "/dev/sda";
-    type = "disk";
-    content = {
-      type = "gpt";
-      partitions = {
-        efi = {
-          size = "512M";
-          type = "EF00";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-          };
-        };
-        root = {
-          size = "100%";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
+_: {
+  # This formats the disk with the ext4 filesystem
+  # Other examples found here: https://github.com/nix-community/disko/tree/master/example
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "100M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "defaults" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+                mountOptions = [ "defaults" ];
+              };
+            };
           };
         };
       };
     };
   };
 }
-
