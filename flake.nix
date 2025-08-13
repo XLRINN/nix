7608fb8d08b638adf82d06455697d556333487d4
david <xlrin.morgan@gmail.com>
2025-08-13 10:48:47 -0400

    feat: add server application to flake.nix for improved deployment options

--- flake.nix
+++ flake.nix
@@ -83,6 +83,7 @@
         "create-keys" = mkApp "create-keys" system;
         "check-keys" = mkApp "check-keys" system;
         "desktop" = mkApp "desktop" system;
+        "server" = mkApp "server" system;
       };
       mkDarwinApps = system: {
         "apply" = mkApp "apply" system;
