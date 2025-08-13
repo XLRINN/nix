0d334c7c5eda4cf30c23d2b7d9c7234d3341f5c3
david <xlrin.morgan@gmail.com>
2025-08-13 10:51:37 -0400

    fix: update download_config function to fetch server-specific configuration and adjust file naming

--- modules/server/files.nix
+++ modules/server/files.nix
@@ -7,37 +7,6 @@ let
   xdg_stateHome  = "${home}/.local/state";
 in
 {
-  "${xdg_dataHome}/bin/movesinks" = {
-    executable = true;
-    text = ''
-      #!/usr/bin/env bash
-      pacmd set-default-sink "$1"
-      pacmd list-sink-inputs | grep index | while read -r line
-      do
-        pacmd move-sink-input "$(echo "$line" | cut -f2 -d' ')" "$1"
-      done
-    '';
-  };
-
-  "${xdg_dataHome}/bin/speakers" = {
-    executable = true;
-    text = ''
-      #!/usr/bin/env bash
-      # Script to change audio format to headphones and check if the sink exists
-
-      # Define the sink name
-      SINK_NAME="alsa_output.usb-Audioengine_Audioengine_2_-00.analog-stereo"
-
-      # Check if the sink exists
-      if pactl list short sinks | grep -q "$SINK_NAME"; then
-        # Sink exists, set it as the default
-        pacmd set-default-sink "$SINK_NAME"
-        movesinks "$SINK_NAME"
-      else
-        echo "Sink $SINK_NAME not found."
-        exit 1
-      fi
-    '';
-  };
+  # Server-specific files can be added here if needed
 }
 
