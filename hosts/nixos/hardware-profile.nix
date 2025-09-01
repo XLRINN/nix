345baa573fc554a12c1f0d7572f09453bb9d1fa6
XLRINN <63594307+XLRINN@users.noreply.github.com>
2025-09-01 15:22:02 +0000

    🔧 Fix imports condition to ensure inputs are checked before accessing hardware profile in NixOS configuration

--- hosts/nixos/hardware-profile.nix
+++ hosts/nixos/hardware-profile.nix
@@ -1,8 +1,8 @@
-{ inputs, lib, ... }:
+{ lib, inputs ? null, ... }:
 let
 	# Default to Framework 13 Intel profile; set to null on non-Framework machines
 	path = "framework/13-inch/intel";
 in
 {
-	imports = lib.optionals (path != null) [ (inputs.nixos-hardware + "/" + path) ];
+	imports = lib.optionals (inputs != null && path != null) [ (inputs.nixos-hardware + "/" + path) ];
 }
