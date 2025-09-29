157e55e0b9963998a462705da3af8d1af74a1e58
XLRINN <63594307+XLRINN@users.noreply.github.com>
2025-09-29 17:50:28 +0000

    fix: Update disk configuration import in NixOS server setup

--- hosts/nixos/server/default.nix
+++ hosts/nixos/server/default.nix
@@ -5,7 +5,7 @@ let
 	keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOk8iAnIaa1deoc7jw8YACPNVka1ZFJxhnU4G74TmS+p" ];
 in
 {
-	imports = [ ../../modules/nixos/disk-config-btrfs.nix ];
+	imports = [ ../../modules/nixos/disk-config.nix ];
 
 	networking = {
 		hostName = "server"; # token replaced by apply script
