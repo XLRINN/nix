48c0cc1836068261625285bd1e075cdc9bb00170
XLRINN <63594307+XLRINN@users.noreply.github.com>
2025-09-29 17:54:00 +0000

    fix: Update imports in NixOS server configuration for hardware and shared modules

--- hosts/nixos/server/default.nix
+++ hosts/nixos/server/default.nix
@@ -5,7 +5,11 @@ let
 	keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOk8iAnIaa1deoc7jw8YACPNVka1ZFJxhnU4G74TmS+p" ];
 in
 {
-	imports = [ ../../modules/nixos/disk-config.nix ];
+	imports = [
+		../../modules/nixos/disk-config.nix
+		../../modules/nixos/hardware.nix
+		../../modules/shared
+	];
 
 	networking = {
 		hostName = "server"; # token replaced by apply script
